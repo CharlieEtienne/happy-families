355ec3ca919cae0056fc601787496b6021267ecc
CHARLIE ETIENNE <ce@web-nancy.fr>
2023-08-25 00:02:06 +0200

    WIP

--- resources/views/livewire/families.blade.php
+++ resources/views/livewire/families.blade.php
@@ -4,7 +4,7 @@
     font: 'Nunito',
     theme: 'colorful',
     verso: false,
-    verso__defaultFontSize: 28,
+    verso__defaultFontSize: 24,
     verso__fontSize: this.defaultFontSize,
     verso__textBoxHeight: 'auto',
     verso__minFontSize: 16,
@@ -13,11 +13,11 @@
     verso__shortTextLimit: 14
 }">
 
-    <div class="drawer lg:drawer-open">
+    <div class="drawer lg:drawer-open lg:auto-cols-auto">
         <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content flex flex-col items-center justify-center">
+        <div class="drawer-content flex flex-col items-center justify-center w-[97vw] lg:w-[calc(100vw-26rem)]">
 
-            <div class="flex py-2 flex-col items-center w-full sticky bg-gray-100 z-50 top-0 box-gradient-shadow-custom">
+            <div class="flex py-2 flex-col items-center w-full sticky bg-gray-100 z-50 top-[64px] box-gradient-shadow-custom">
                 <label for="verso" class="cursor-pointer label">
 
                     <span class="label-text" :class="!verso ? 'text-primary mx-1 font-extrabold relative inline-block stroke-current' : ''">
@@ -43,7 +43,7 @@
 
             {{-- <button wire:click="generate()">DEBUG</button> --}}
 
-            <div class="p-8 bg-white w-full">
+            <div class="p-8 bg-white h-full w-full overflow-x-scroll">
 
                 {{-- =============================================================== --}}
                 {{-- ============================ RECTO ============================ --}}
@@ -60,7 +60,7 @@
                      x-transition:leave-end="opacity-0 transform -translate-x-12">
                     @for ($current_family = 1; $current_family <= $families_count; $current_family++)
                         @for ($family_member = 1; $family_member <= $members_per_family; $family_member++)
-                            <div class="card bg{{$current_family}} text-white" style="min-height: 9cm;" :style="{ 'border-radius': radius+'px' }">
+                            <div class="card bg{{$current_family}} text-white w-[5.71cm] h-[8.89cm] m-auto" :style="{ 'border-radius': radius+'px' }">
                                 <div class="card-body justify-between" :style="{ 'padding': padding+'px' }">
 
                                     <h2 class="card-title text-center self-center">
@@ -73,15 +73,16 @@
                                                   x-on:input="handleInput"
                                                   x-bind:style="{ fontSize: fontSize + 'px', height: textBoxHeight }"
                                                   placeholder="Click to edit"
-                                                  class="input input-ghost bg-opacity-0 w-full max-w-xs text-center self-center text-3xl placeholder-gray-200 resize-none overflow-hidden"
+                                                  class="input input-ghost bg-opacity-0 w-full max-w-xs text-center self-center text-2xl placeholder-gray-200 resize-none overflow-hidden"
                                                   id="families.{{$current_family}}.family_members.{{$family_member}}.name"
+                                                  wire:key="families.{{$current_family}}.family_members.{{$family_member}}"
                                                   wire:model.blur="families.{{$current_family}}.family_members.{{$family_member}}.name">
                                         </textarea>
                                     </label>
 
                                     <div class="card-actions overflow-hidden">
                                         @for ($members_pill = 1; $members_pill <= $members_per_family; $members_pill++)
-                                            <div class="badge bg{{$current_family}}-alt border-white text-white overflow-hidden min-h-6 h-auto max-w-full inline-block whitespace-nowrap overflow-ellipsis">
+                                            <div class="badge bg{{$current_family}}-alt border-white text-white overflow-hidden h-auto max-w-full inline-block whitespace-nowrap overflow-ellipsis text-xs">
                                                 {{$families[$current_family]['family_members'][$members_pill]['name'] ?? "Name #" . $members_pill}}
                                             </div>
                                         @endfor
@@ -110,7 +111,7 @@
                      x-ref="versoTextareasContainer">
                     @for ($current_family = 1; $current_family <= $families_count; $current_family++)
                         @for ($family_member = 1; $family_member <= $members_per_family; $family_member++)
-                            <div class="card bg-primary text-white" style="min-height: 9cm;" :style="{ 'border-radius': radius+'px' }">
+                            <div class="card bg-primary text-white w-[5.71cm] h-[8.89cm] m-auto" :style="{ 'border-radius': radius+'px' }">
                                 <div class="card-body justify-center" :style="{ 'padding': padding+'px' }">
                                     <label>
                                         <textarea
@@ -119,7 +120,7 @@
                                             x-on:input="handleInput"
                                             x-bind:style="{ fontSize: verso__fontSize + 'px', height: verso__textBoxHeight }"
                                             placeholder="Click to edit"
-                                            class="verso-textbox input input-ghost bg-opacity-0 w-full max-w-xs text-center self-center text-3xl placeholder-gray-200 resize-none overflow-hidden"
+                                            class="verso-textbox input input-ghost bg-opacity-0 w-full max-w-xs text-center self-center text-2xl placeholder-gray-200 resize-none overflow-hidden"
                                             id="verso_text_families.{{$current_family}}.family_members.{{$family_member}}"
                                             wire:model.debounce.500ms="verso_text">
                                         </textarea>
@@ -136,101 +137,110 @@
         {{-- =============================================================== --}}
         {{-- ======================= SETTINGS SIDEBAR ====================== --}}
         {{-- =============================================================== --}}
-        <div class="drawer-side sm:px-8 sm:py-4 sm:bg-gray-100 sm:max-w-md sm:shadow-[0_0_10px_0_rgba(0,0,0,0.3)] z-50">
+        <div class="drawer-side z-50 sm:shadow-[0_0_10px_0_rgba(0,0,0,0.3)]">
             <label for="my-drawer-2" class="drawer-overlay"></label>
 
-            <x-cool-underline-title :type="'h2'" class="text-xl text-primary font-extrabold text-center mb-6">Settings</x-cool-underline-title>
+            <div class="px-8 py-4 pb-12 bg-gray-100 max-w-md shadow-[0_0_10px_0_rgba(0,0,0,0.3)]">
 
-            {{-- Number of families --}}
-            <div class="flex mt-2 items-center" x-data>
-                <label for="families_count" class="label w-[130px]">
-                    <span class="label-text">Number of families</span>
-                </label>
-                <input type="range" min="1" max="10"
-                       class="range range-xs range-primary ml-4"
-                       id="families_count"
-                       wire:model.live.debounce.250="families_count"/>
-                <span class="label-text ml-3" x-text="$wire.families_count"></span>
+                <div class="flex w-full justify-end">
+                    <label for="my-drawer-2" class="group absolute cursor-pointer p-1 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-900 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-white">
+                        <svg class="fill-current group-hover:scale-125 transition" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
+                    </label>
+                </div>
+                <x-cool-underline-title :type="'h2'" class="text-xl text-primary font-extrabold text-center mb-6">Settings</x-cool-underline-title>
+
+                {{-- Number of families --}}
+                <div class="flex mt-2 items-center" x-data>
+                    <label for="families_count" class="label w-[130px]">
+                        <span class="label-text">Number of families</span>
+                    </label>
+                    <input type="range" min="1" max="10"
+                           class="range range-xs range-primary ml-4"
+                           id="families_count"
+                           wire:model.live.debounce.250="families_count"/>
+                    <span class="label-text ml-3" x-text="$wire.families_count"></span>
+
+                </div>
+
+                {{-- Members for each family --}}
+                <div class="flex mt-2 items-center">
+                    <label for="members_per_family" class="label w-[130px]">
+                        <span class="label-text">Members for each family?</span>
+                    </label>
+                    <input type="range" min="1" max="10"
+                           class="range range-xs range-primary ml-4"
+                           id="members_per_family"
+                           wire:model.live.debounce.250="members_per_family"/>
+                    <span class="label-text ml-3" x-text="$wire.members_per_family"></span>
+                </div>
+
+                <div class="alert bg-info/10 my-3 shadow-md border-info py-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <div class="text-sm">This will generate <span class="font-semibold underline underline-offset-2 decoration-2 decoration-sky-500/30" x-text="$wire.families_count * $wire.members_per_family"></span> cards</div>
+                </div>
+
+                {{-- Theme setting --}}
+                <div class="flex mt-2 items-center">
+                    <label for="theme" class="label w-[130px]">
+                        <span class="label-text">Theme</span>
+                    </label>
+                    <select x-model="theme" id="theme" class="select select-bordered select-sm ml-4 grow">
+                        <option value="colorful" selected>Colorful</option>
+                        <option value="gradient">Gradient</option>
+                        <option value="light">Light</option>
+                    </select>
+                </div>
+
+                {{-- Font family setting --}}
+                <div class="flex mt-2 items-center relative z-50">
+                    <label for="border-radius" class="label w-[130px]">
+                        <span class="label-text">Font family</span>
+                    </label>
+                    <div class="ml-4" id="font-picker" wire:ignore></div>
+                </div>
+
+                {{-- Border Radius setting --}}
+                <div class="flex mt-2 items-center">
+                    <label for="border-radius" class="label w-[130px]">
+                        <span class="label-text">Border Radius (px)</span>
+                    </label>
+                    <input
+                        class="range range-xs range-primary ml-4"
+                        type="range" min="0" max="50" x-model="radius" id="border-radius">
+                    <span class="label-text ml-3" x-text="radius + 'px'"></span>
+                </div>
+
+                {{-- Padding setting --}}
+                <div class="flex mt-2 items-center">
+                    <label for="padding" class="label w-[130px]">
+                        <span class="label-text">Padding (px)</span>
+                    </label>
+                    <input
+                        class="range range-xs range-primary ml-4"
+                        type="range" min="0" max="50" x-model="padding" id="padding">
+                    <span class="label-text ml-3" x-text="padding + 'px'"></span>
+                </div>
+
+
+
+                <x-cool-underline-title :type="'h3'" class="text-lg text-gray-600 font-extrabold mt-6 mb-2">Families</x-cool-underline-title>
+
+                <form wire:submit>
+                    @for ($current_family = 1; $current_family <= $families_count; $current_family++)
+                        <div class="flex">
+                            <label for="family.{{$current_family}}.name" class="label w-[130px]">
+                                <span class="label-text">Name of family #{{$current_family}}</span>
+                            </label>
+                            <input
+                                class="input input-bordered input-sm max-w-md ml-4 mt-2"
+                                id="family.{{$current_family}}.name"
+                                type="text"
+                                wire:model.blur="families.{{$current_family}}.name" >
+                        </div>
+                    @endfor
+                </form>
 
             </div>
-
-            {{-- Members for each family --}}
-            <div class="flex mt-2 items-center">
-                <label for="members_per_family" class="label w-[130px]">
-                    <span class="label-text">Members for each family?</span>
-                </label>
-                <input type="range" min="1" max="10"
-                       class="range range-xs range-primary ml-4"
-                       id="members_per_family"
-                       wire:model.live.debounce.250="members_per_family"/>
-                <span class="label-text ml-3" x-text="$wire.members_per_family"></span>
-            </div>
-
-            <div class="alert bg-info/10 my-3 shadow-md border-info py-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                <div class="text-sm">This will generate <span class="font-semibold underline underline-offset-2 decoration-2 decoration-sky-500/30" x-text="$wire.families_count * $wire.members_per_family"></span> cards</div>
-            </div>
-
-            {{-- Theme setting --}}
-            <div class="flex mt-2 items-center">
-                <label for="theme" class="label w-[130px]">
-                    <span class="label-text">Theme</span>
-                </label>
-                <select x-model="theme" id="theme" class="select select-bordered select-sm ml-4 grow">
-                    <option value="colorful" selected>Colorful</option>
-                    <option value="gradient">Gradient</option>
-                    <option value="light">Light</option>
-                </select>
-            </div>
-
-            {{-- Font family setting --}}
-            <div class="flex mt-2 items-center relative z-50">
-                <label for="border-radius" class="label w-[130px]">
-                    <span class="label-text">Font family</span>
-                </label>
-                <div class="ml-4" id="font-picker" wire:ignore></div>
-            </div>
-
-            {{-- Border Radius setting --}}
-            <div class="flex mt-2 items-center">
-                <label for="border-radius" class="label w-[130px]">
-                    <span class="label-text">Border Radius (px)</span>
-                </label>
-                <input
-                    class="range range-xs range-primary ml-4"
-                    type="range" min="0" max="50" x-model="radius" id="border-radius">
-                <span class="label-text ml-3" x-text="radius + 'px'"></span>
-            </div>
-
-            {{-- Padding setting --}}
-            <div class="flex mt-2 items-center">
-                <label for="padding" class="label w-[130px]">
-                    <span class="label-text">Padding (px)</span>
-                </label>
-                <input
-                    class="range range-xs range-primary ml-4"
-                    type="range" min="0" max="50" x-model="padding" id="padding">
-                <span class="label-text ml-3" x-text="padding + 'px'"></span>
-            </div>
-
-
-
-            <x-cool-underline-title :type="'h3'" class="text-lg text-gray-600 font-extrabold mt-6 mb-2">Families</x-cool-underline-title>
-
-            <form wire:submit>
-                @for ($current_family = 1; $current_family <= $families_count; $current_family++)
-                    <div class="flex">
-                        <label for="family.{{$current_family}}.name" class="label w-[130px]">
-                            <span class="label-text">Name of family #{{$current_family}}</span>
-                        </label>
-                        <input
-                            class="input input-bordered input-sm max-w-md ml-4 mt-2"
-                            id="family.{{$current_family}}.name"
-                            type="text"
-                            wire:model.blur="families.{{$current_family}}.name" >
-                    </div>
-                @endfor
-            </form>
 
         </div>
 
@@ -239,7 +249,7 @@
     <script>
         document.addEventListener('alpine:init', () => {
             Alpine.data('textResize', (side) => ({
-                defaultFontSize: 28,
+                defaultFontSize: 24,
                 fontSize: this.defaultFontSize,
                 textBoxHeight: 'auto',
                 minFontSize: 16,
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="antialiased">
 
-        <div class="navbar bg-neutral text-neutral-content">
+        <div class="navbar bg-neutral text-neutral-content sticky top-0 z-[60]">
             <div class="navbar-start">
                 <label for="my-drawer-2" tabindex="0" class="btn btn-ghost btn-circle drawer-button">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
