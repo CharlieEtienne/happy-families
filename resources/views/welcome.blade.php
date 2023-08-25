7c72fbc6ed439e1fde7bb63d86fbd7822493af08
CHARLIE ETIENNE <ce@web-nancy.fr>
2023-08-25 16:07:55 +0200

    WIP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="antialiased">
 
-        <div class="navbar bg-neutral text-neutral-content sticky top-0 z-[60]">
+        <div class="print:hidden navbar bg-neutral text-neutral-content sticky top-0 z-[60]">
             <div class="navbar-start">
                 <label for="my-drawer-2" tabindex="0" class="btn btn-ghost btn-circle drawer-button">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
@@ -32,7 +32,7 @@
             <livewire:families/>
         </div>
 
-        <footer class="footer items-center p-4 bg-neutral text-neutral-content">
+        <footer class="print:hidden footer items-center p-4 bg-neutral text-neutral-content">
             <div class="items-center grid-flow-col">
                 <div class="avatar">
                     <div class="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
@@ -46,6 +46,5 @@
                 <a title="Twitter" target="_blank" href="https://twitter.com/charlie_etienne"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
             </div>
         </footer>
-
     </body>
 </html>
